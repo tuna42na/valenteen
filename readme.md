c15c2f3cf205848aaa60d5d9ca36138822deabeb
Tuna Fortuna <tuna42na@gmail.com>
2020-09-01 16:55:59 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Valen-Teens
+
+A Valentine's Day Matchmacking app for the students of Wolfson Highschool
+
+# Features
+
+- Ability to Add Quizzes to Database
+- Create Account for Students to take quizzes
+- View of 10 Most Compatibile Students and 10 Least Compatabile Students
+
+# Stack
+
+- MongoDB
+- ExpressJS
+- ReactJS
+- NodeJS
---
+++ server.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+var express = require("express");
+var app = express();
+var expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", (error) => console.log("Conneted to Mongoose"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
